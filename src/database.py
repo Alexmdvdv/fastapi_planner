fc5ff2a00f13dc2a3bf37f345f4145a6b1597b41
Alexmdvdv <alexandrmdvdv@mail.ru>
2023-10-28 11:46:04 +0500

    replaced SQLite with PostgreSQL

--- src/database.py
+++ src/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
